25ce67978747dcbef59cf23c8e39fe93e880de55
kadircanarici <kadircanarc@gmail.com>
2023-02-09 17:07:29 +0300

    book operations

---
+++ BookStore.API/Application/BookOperations/Commands/DeleteBook/DeleteBookCommand.cs
@@ -0,0 +1,26 @@
+﻿using BookStore.API.DBOperations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API.Application.BookOperations.Commands.DeleteBook
+{
+    public class DeleteBookCommand
+    {
+        public readonly BookStoreDBContext _dbContext;
+        public int BookId { get; set; }
+        public DeleteBookCommand(BookStoreDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Handle()
+        {
+
+            var book = _dbContext.Books.SingleOrDefault(x => x.Id == BookId);
+            if (book == null)
+                throw new InvalidOperationException("Silinecek Kitap Bulunamadı");
+            _dbContext.Books.Remove(book);
+            _dbContext.SaveChanges();
+
+
+        }
+    }
+}
---
+++ BookStore.API/Application/BookOperations/Queries/GetBooks/GetBooksQuery.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using BookStore.API.Common;
+using BookStore.API.DBOperations;
+using BookStore.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API.Application.BookOperations.Queries.GetBooks
+{
+    public class GetBooksQuery
+    {
+        private readonly BookStoreDBContext _dbContext;
+        private readonly IMapper _mapper;
+        public GetBooksQuery(BookStoreDBContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+        public List<BooksViewModel> Handle()
+        {
+            var bookList = _dbContext.Books.OrderBy(x => x.Id).ToList();
+            List<BooksViewModel> wm = _mapper.Map<List<BooksViewModel>>(bookList);  //new List<BooksViewModel>();
+            //foreach (var book in bookList)
+            //{
+            //    wm.Add(new BooksViewModel()
+            //    {
+            //        Title = book.Title,
+            //        Genre = ((GenreEnum)book.GenreId).ToString(),
+            //        PublishDate = book.PublishDate.Date.ToString("dd/MM/yyyy"),
+            //        PageCount = book.PageCount
+            //    });
+            //}
+            return wm;
+        }
+    }
+
+    public class BooksViewModel
+    {
+        public string Title { get; set; }
+        public int PageCount { get; set; }
+        public string PublishDate { get; set; }
+        public string Genre { get; set; }
+    }
+}
